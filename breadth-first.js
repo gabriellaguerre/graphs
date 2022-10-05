a1f0634ba2467c5f539004868bd01d4d3ee76de6
gabriellaguerre <laguerre.g.g@gmail.com>
2022-10-05 09:06:41 -0400

    breadth-first simple

---
+++ breadth-first.js
@@ -0,0 +1,43 @@
+// Write a function called printBreadthFirst that will traverse the given graph breadth-first,
+// printing each node when visited exactly once, on a newline.
+
+const adjList = {
+    1: [2, 5],
+    2: [1, 3, 5],
+    3: [2, 4],
+    4: [3, 5, 6],
+    5: [1, 2, 4],
+    6: [4]
+}
+
+function printBreadthFirst(start) {
+    let queue = [start]
+
+    let visited = new Set()
+    visited.add(start)
+
+    while (queue.length) {
+        const node = queue.shift()
+
+        console.log(node)
+
+        let neighbors = adjList[node]
+
+        for(neighbor of neighbors)  {
+            if (!visited.has(neighbor)) {
+                visited.add(neighbor)
+                queue.push(neighbor)
+            }
+        }
+    }
+}
+
+console.log("First Test:")
+printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
+// One possible solution:  3, 2, 4, 1, 5, 6
+console.log("Second Test:")
+printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
+// One possible solution:  6, 4, 3, 5, 2, 1
+console.log("Third Test:")
+printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
+// One possible solution:  4, 3, 5, 6, 2, 1
